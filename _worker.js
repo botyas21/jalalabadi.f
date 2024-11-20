8d4d7fd77ea0e77b1efcfacf8ec4259ec0f9924c
mohsen <144608092+botyas21@users.noreply.github.com>
2024-11-20 20:27:47 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,8 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'c51e8216-08e6-4063-a85d-734001695621';
+let userID = '174c246b-665d-49dd-bcbf-19d32fe06344
+';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
